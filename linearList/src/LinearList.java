c202d6c6b3df5e1c8e36612953248d7114b9e2b9
LiuXPeng <703762445@qq.com>
2019-12-11 20:22:31 +0800

    1

---
+++ linearList/src/LinearList.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public abstract class LinearList <E>{
+//    public abstract void linklist();
+
+    public abstract void linklist(List<E> c);
+    public abstract E LinearList();
+}
