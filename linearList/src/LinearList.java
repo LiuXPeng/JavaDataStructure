08034e1f9df1d24dfd5c02995cead29e4e8ef7fb
LiuXPeng <703762445@qq.com>
2019-12-11 22:16:35 +0800

    11

--- linearList/src/LinearList.java
+++ linearList/src/LinearList.java
@@ -1,8 +1,25 @@
 import java.util.List;
 
-public abstract class LinearList <E>{
-//    public abstract void linklist();
-
-    public abstract void linklist(List<E> c);
-    public abstract E LinearList();
+public abstract class LinearList <T>{
+    public abstract void linklist();
+    public abstract void linklist(List<T> c);
+    public abstract T getfirst();
+    public abstract T getlast();
+    public abstract T removefirst();
+    public abstract T removelast();
+    public abstract void addfirst(T o);
+    public abstract void addlast(T o);
+    public abstract boolean contains(T o);
+    public abstract int size();
+    public abstract boolean add(T o);
+    public abstract boolean remove(T o);
+    public abstract void addall(List<T> c);
+    public abstract boolean addall(int index, List<T> c);
+    public abstract void clear();
+    public abstract T get(int index);
+    public abstract void set(int index, T o);
+    public abstract void add(int index, T o);
+    public abstract void remove(int index);
+    public abstract int indexof(T o);
+    public abstract int lastindexof(T o);
 }
---
+++ linearList/src/SingleLinkedList.java
@@ -0,0 +1,108 @@
+import java.util.List;
+
+public class SingleLinkedList extends LinearList{
+    @Override
+    public void linklist() {
+
+    }
+
+    @Override
+    public void linklist(List c) {
+
+    }
+
+    @Override
+    public Object getfirst() {
+        return null;
+    }
+
+    @Override
+    public Object getlast() {
+        return null;
+    }
+
+    @Override
+    public Object removefirst() {
+        return null;
+    }
+
+    @Override
+    public Object removelast() {
+        return null;
+    }
+
+    @Override
+    public void addfirst(Object o) {
+
+    }
+
+    @Override
+    public void addlast(Object o) {
+
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public void addall(List c) {
+
+    }
+
+    @Override
+    public boolean addall(int index, List c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public void set(int index, Object o) {
+
+    }
+
+    @Override
+    public void add(int index, Object o) {
+
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public int indexof(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastindexof(Object o) {
+        return 0;
+    }
+}
